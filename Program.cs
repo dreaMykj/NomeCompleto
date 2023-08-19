e5a69b226624b8531b6d5691068654effc461845
dreaMykj <gabrielgreghi2018@gmail.com>
2023-08-18 21:34:47 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Olá usuário, \nQual o seu nome? ");
+string nome;
+nome = Console.ReadLine()!;
+
+Console.Write("Qual o seu sobrenome? ");
+
+string sobrenome;
+sobrenome = Console.ReadLine()!;
+
+Console.Write("Boa noite. ");
+Console.Write (nome);
+Console.Write (sobrenome);
+Console.Write(" ");
+Console.WriteLine(".");
